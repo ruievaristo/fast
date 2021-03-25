280fb10c432c8a65c60d60667989d1a52746114e
Rui Evaristo <ruiaevaristo@gmail.com>
2021-03-25 19:05:24 +0000

    brothers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Prism by TEMPLATED
+	Radius by TEMPLATED
 	templated.co @templatedco
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
@@ -17,7 +17,9 @@
 	$(function() {
 
 		var	$window = $(window),
-			$body = $('body');
+			$body = $('body'),
+			$header = $('#header'),
+			$footer = $('#footer');
 
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
@@ -37,6 +39,40 @@
 					'.important\\28 medium\\29',
 					skel.breakpoint('medium').active
 				);
+			});
+
+		// Header.
+			$header.each( function() {
+
+				var t 		= jQuery(this),
+					button 	= t.find('.button');
+
+				button.click(function(e) {
+
+					t.toggleClass('hide');
+
+					if ( t.hasClass('preview') ) {
+						return true;
+					} else {
+						e.preventDefault();
+					}
+
+				});
+
+			});
+
+		// Footer.
+			$footer.each( function() {
+
+				var t 		= jQuery(this),
+					inner 	= t.find('.inner'),
+					button 	= t.find('.info');
+
+				button.click(function(e) {
+					t.toggleClass('show');
+					e.preventDefault();
+				});
+
 			});
 
 	});
